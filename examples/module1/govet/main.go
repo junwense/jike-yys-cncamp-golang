5820e262c634ca723c336cbcea0035260fd068b9
seanjiao <970597653@qq.com>
2022-07-20 01:28:28 +0800

    commit govet

--- examples/module1/govet/main.go
+++ examples/module1/govet/main.go
@@ -6,6 +6,11 @@ import (
 
 func main() {
 	name := "testing"
+	s := "map be use these"
 	fmt.Printf("%d\n", name)
 	fmt.Printf("%s\n", name, name)
+
+	fmt.Printf("%s\n", s)
+	fmt.Printf("%s\n", name)
+	fmt.Printf("%s%s\n", name, name)
 }
